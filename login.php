77dde2b5d3e5d9228f7cbc90b26d8e8a78ec81df
Ellyn737 <e.reese@mail.de>
2017-12-21 10:11:35 +0100

    changed font and buttons

--- login.php
+++ login.php
@@ -1,8 +1,20 @@
 <?php
     
-    $servername = "localhost";
-    $username = "root";
-    $password = "password";
-    $dbname = ""
-	
+    $servername = "pstud0.mt.haw-hamburg.de";
+    $username = "aby369";
+    $password = "aby369";
+    $dbname = "aby369"
+
+        
+    //create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    //check connection
+    if($conn -> connect_error){
+        die("Connection failed: " .$conn -> connect_error);
+    }
+    else{
+        echo("Connected");
+    }
+    
 ?>
